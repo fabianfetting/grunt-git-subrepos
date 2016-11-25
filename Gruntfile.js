6afd6c29a09ac037608523fe2963c6a152a97895
Fabian Fetting <mail@fabianfetting.de>
2016-11-25 21:21:08 +0100

    Add subrepo task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,67 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    git_subrepos: {
-      default_options: {
-        options: {
+    // Project configuration.
+    grunt.initConfig({
+        // Configuration to be run (and then tested).
+        subrepos: {
+            options: { // global
+                force: false,
+            },
+            example1: {
+                options: { // local
+                    force: true,
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'testing2',
+                        src: 'inner',
+                        repoName: 'test',
+                        repo: 'url://test',
+                    },
+                    {
+                        expand: true,
+                        cwd: 'testing',
+                        src: '/',
+                        repo: 'url://test',
+                        branch: 'develop'
+                    },
+                    {
+                        src: 'testing2/inner',
+                        repoName: 'test',
+                        repo: 'url://test',
+                    },
+                ],
+                // files: [
+                //     {
+                //         cwd: 'test/',
+                //         ex: {
+                //             repository: 'git@github.com:fur6y/example1.git',
+                //             branch: 'feature/example1',
+                //             directory: 'example1',
+                //             remote: 'origin',
+                //         },
+                //     },
+                //     {
+                //         cwd: 'test/',
+                //         repository: 'git@github.com:fur6y/example1.git',
+                //         branch: 'feature/example1',
+                //         directory: 'example1',
+                //         remote: 'origin',
+                //     },
+                // ]
+            },
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    });
 
-  });
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'git_subrepos', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['subrepos']);
 
 };
---
+++ tasks/subrepos.js
@@ -0,0 +1,117 @@
+/*
+ * grunt-git-subrepos
+ * https://github.com/fur6y/grunt-git-subrepos
+ *
+ * Copyright (c) 2016 Fabian Fetting
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    var git = {
+        isRepo: function(path, doneCallback) {
+            grunt.log.debug('Cwd:', process.cwd() + '/' + path);
+            grunt.log.debug('Spawn cmd:', 'git rev-parse --is-inside-work-tree --quiet');
+
+            grunt.util.spawn({
+                cmd: 'git',
+                args: ['rev-parse', '--quiet', '--is-inside-work-tree'],
+                opts: { cwd: process.cwd() + '/' + path },
+            }, function (error, result, code) {
+                doneCallback(code === 0 && result === 'true');
+            });
+        },
+        isUntouched: function (path, doneCallback) {
+            doneCallback(true);
+        },
+        clone: function (path, repo, branch, doneCallback) {
+            var args = [];
+            args.push('clone', '--quiet');
+            if (branch) { args.push('--branch', branch); }
+            args.push(repo, path);
+
+            grunt.log.debug('Spawn cmd:', 'git', args);
+
+            if (grunt.option('no-write')) { return doneCallback(); }
+
+            grunt.util.spawn({
+                cmd: 'git',
+                args: args,
+            }, doneCallback);
+        },
+        checkout: function(path, branch, doneCallback) {
+            grunt.log.debug('Spawn cmd:', 'git checkout --quiet ' + branch);
+            doneCallback();
+        },
+        pull: function(path, doneCallback) {
+            grunt.log.debug('Spawn cmd:', 'git pull --quiet');
+            doneCallback();
+        },
+    };
+
+    grunt.registerMultiTask('subrepos', 'Description.', function () {
+        var done = this.async();
+
+        var options = this.options({
+            force: false,
+        });
+
+        this.files.forEach(function (file) {
+            var repo = file.repo;
+            var repoName = file.repoName || '';
+            var branch = file.branch || '';
+
+            grunt.verbose.writeln('');
+            grunt.verbose.write('Repo:', '\'' + repo.cyan + '\'');
+            grunt.verbose.write(', ');
+            grunt.verbose.write('RepoName:', '\'' + repoName.cyan + '\'');
+            grunt.verbose.write(', ');
+            grunt.verbose.writeln('Branch:', '\'' + branch.cyan + '\'');
+
+            file.src.forEach(function (filePath, i) {
+                filePath += repoName || '';
+                grunt.verbose.writeln('Path: ' + filePath.cyan);
+
+                if (!grunt.file.isDir(filePath)) {
+                    grunt.log.debug('Path ' + filePath + ' NOT exists.');
+                    grunt.file.mkdir(filePath);
+                    git.clone(filePath, repo, branch, function () {
+                        grunt.verbose.ok();
+                    });
+
+                } else {
+                    grunt.log.debug('Path ' + filePath + ' exists.');
+
+                    git.isRepo(filePath, function (isRepo) {
+                        if (isRepo) {
+                            git.isUntouched(filePath, function (isUntouched) {
+                                if (isUntouched) {
+                                    grunt.log.debug('Git repo exists.');
+
+                                    git.checkout(filePath, branch, function () {
+                                        git.pull(filePath, function () {
+                                            grunt.verbose.ok();
+                                        });
+                                    });
+
+                                } else {
+                                    grunt.log.errorlns('Could\'t update repo because of local changes.');
+                                }
+                            });
+
+                        } else {
+                            grunt.fail.warn('Path ' + filePath + ' exists but is not a git repository.');
+                            grunt.verbose.ok();
+                        }
+                    });
+
+                }
+            });
+        });
+
+        done();
+    });
+
+};
